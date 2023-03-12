333b815f8f0df4a1afe4cf26c18aa467ab3d78c5
Constantin Pilsl <constantin.pilsl@posteo.de>
2023-03-12 17:49:01 +0100

    Workable version that will be used to implement the api calls

--- publisher/button_test.py
+++ publisher/button_test.py
@@ -1,8 +1,8 @@
 import asyncio
 import logging
-import os.path
+import os
 from operator import itemgetter
-from typing import Any
+from typing import Any, Union
 
 from aiogram import Bot, Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
@@ -18,12 +18,35 @@ from aiogram_dialog import (
 )
 from aiogram_dialog.manager.protocols import ManagedDialogAdapterProto
 from aiogram_dialog.widgets.input import MessageInput
-from aiogram_dialog.widgets.kbd import Back, Button, Multiselect, Row, Select, SwitchTo
+from aiogram_dialog.widgets.kbd import (
+    Back,
+    Button,
+    Group,
+    Multiselect,
+    Row,
+    Select,
+    ScrollingGroup,
+    SwitchTo,
+)
 from aiogram_dialog.widgets.text import Const, Format, Multi
 
-src_dir = os.path.normpath(os.path.join(__file__, os.path.pardir))
 
 API_TOKEN = os.environ["TELEGRAM_NEWS_TICKER_DEV_BOT_TOKEN"]
+
+application_data = {
+    "items": [
+        "Ukraine",
+        "USA",
+        "Deutschland",
+        "Technologie",
+        "Silion Valley",
+        "Hard Rock Cafe",
+    ],
+}
+
+user_data = {
+    "keywords": None,
+}
 
 
 class DialogSG(StatesGroup):
@@ -31,124 +54,132 @@ class DialogSG(StatesGroup):
     main_menu = State()
     finish = State()
     get_news = State()
-    set_keywords = State()
-    list_keywords = State()
+    add_keywords = State()
+    edit_keywords = State()
 
 
-async def get_keywords_data(dialog_manager: DialogManager, **kwargs):
-    keywords = dialog_manager.current_context().dialog_data.get("keywords", None)
+
+# CONTROLLER
+async def get_keywords_enumerated(dialog_manager: DialogManager, **kwargs):
+    enumerated_keywords = [(item, i) for i, item in enumerate(application_data["items"])]
     return {
-        "keywords": keywords,
+        "enumerated_keywords": enumerated_keywords,
     }
 
 
-async def start_handler(
-    m: Message, dialog: ManagedDialogAdapterProto, manager: DialogManager
-):
-    if manager.is_preview():
-        await dialog.next()
-        return
-    await dialog.next()
+async def get_user_keywords(dialog_manager: DialogManager, **kwargs):
+    return {
+        "user_keywords": user_data["keywords"],
+    }
 
 
+async def get_user_keywords_enumerated(dialog_manager: DialogManager, **kwargs):
+    user_keywords_enumerated = [(item, i) for i, item in enumerate(user_data["keywords"])]
+    return {
+        "user_keywords_enumerated": user_keywords_enumerated,
+    }
+
+
+# EVENT HANDLERS
 async def on_submit(c: CallbackQuery, button: Button, manager: DialogManager):
     if manager.is_preview():
         await manager.done()
         return
 
     mselect = manager.current_context().widget_data["mselect"]
+    selected_ids = [int(id) for id in mselect]
 
-    print(mselect)
-    item_ids = [item for item in mselect]
-    item_id_str = ",".join(str(i) for i in item_ids)
+    item_id_mapping = ((i, item) for i, item in enumerate(application_data["items"]))
+    user_data["keywords"] = [i[1] for i in item_id_mapping if i[0] in selected_ids]
 
-    manager.current_context().dialog_data["keywords"] = item_id_str
+    item_id_str = ", ".join([i for i in user_data.get("keywords")])
+    logging.info(f"Keyword ids set to:  {item_id_str}")
+
     await c.message.answer(f"Your keywords have been set to: {item_id_str}")
-    await manager.dialog().next()
+    await manager.done()
 
 
 async def on_finish(c: CallbackQuery, button: Button, manager: DialogManager):
     if manager.is_preview():
         await manager.done()
         return
-    await c.message.answer("Thank you. To start again click /set-keywords")
+    # await c.message.answer("Fuck you!")
     await manager.done()
 
 
-items = [("Ukraine", 1), ("USA", 2), ("Deutschland", 3), ("Technologie", 4)]
-
-multiselect = Multiselect(
-    checked_text=Format("✓ {item[0]}"),
-    unchecked_text=Format("{item[0]}"),
-    id="mselect",
-    item_id_getter=itemgetter(1),
-    items=items,
-)
-
+# VIEWS
 dialog = Dialog(
     Window(
-        SwitchTo(
-            Const("Greetings! Please, introduce yourself:"),
-            id="start",
-            state=DialogSG.main_menu,
-        ),
-        state=DialogSG.start,
-    ),
-    # Window(
-    #     Row(
-    #         Button(Const("Get News"), id="get_news"),  # TODO:  SwitchTo ones it is implemented
-    #         SwitchTo(Const("Start >"), id="start", state=DialogSG.main_menu),
-    #     ),
-    #     state=DialogSG.start,
-    # ),
-    Window(
+        Const("Start >"),
         Row(
             Button(
                 Const("Get News"), id="get_news"
             ),  # TODO:  SwitchTo ones it is implemented
             SwitchTo(
-                Const("Set Keywords"), id="set_keywords", state=DialogSG.set_keywords
+                Const("Add Keywords"), id="add_keywords", state=DialogSG.add_keywords
             ),
             SwitchTo(
-                Const("List Keywords"), id="list_keywords", state=DialogSG.list_keywords
+                Const("Edit Keywords"), id="edit_keywords", state=DialogSG.edit_keywords
             ),
         ),
         Row(
             Back(),
-            SwitchTo(Const("Home"), id="start", state=DialogSG.start),
+            SwitchTo(Const("Home"), id="start", state=DialogSG.main_menu),
+            Button(Const("End"), on_click=on_finish, id="finish"),
         ),
         state=DialogSG.main_menu,
     ),
     Window(
         Const("Please select your keywords:"),
-        multiselect,
+        ScrollingGroup(
+            Multiselect(
+                checked_text=Format("✓ {item[0]}"),
+                unchecked_text=Format("{item[0]}"),
+                id="mselect",
+                item_id_getter=itemgetter(1),
+                items="enumerated_keywords",
+            ),
+            width=2,
+            height=4,
+            id="add_keywords",
+        ),
         Row(
             Back(),
-            SwitchTo(Const("Restart"), id="restart", state=DialogSG.set_keywords),
-            Button(Const("Submit"), on_click=on_submit, id="submit"),
+            Button(Const("✓ Submit"), on_click=on_submit, id="submit"),
+            Button(Const("x Discard"), on_click=on_finish, id="finish"),
         ),
-        getter=get_keywords_data,
-        state=DialogSG.set_keywords,
+        getter=get_keywords_enumerated,
+        state=DialogSG.add_keywords,
     ),
     Window(
-        Multi(
-            Format("Your keywords have been set"),
-            sep="\n\n",
+        # TODO:  EDIT KEYWORDS SHOULD HAVE SELECTED KEYWORDS ALREADY CHECKED
+        Const("Your keywords are:"),
+        ScrollingGroup(
+            Multiselect(
+                checked_text=Format("✓ {item[0]}"),
+                unchecked_text=Format("{item[0]}"),
+                id="mselect",
+                item_id_getter=itemgetter(1),
+                items="user_keywords_enumerated",
+            ),
+            width=2,
+            height=4,
+            id="add_keywords",
         ),
         Row(
             Back(),
-            SwitchTo(Const("Restart"), id="restart", state=DialogSG.set_keywords),
-            Button(Const("Finish"), on_click=on_finish, id="finish"),
+            Button(Const("✓ Submit"), on_click=on_submit, id="submit"),
+            Button(Const("x Discard"), on_click=on_finish, id="finish"),
         ),
-        getter=get_keywords_data,
-        state=DialogSG.finish,
+        getter=get_user_keywords_enumerated,
+        state=DialogSG.edit_keywords,
     ),
 )
 
 
 async def start(m: Message, dialog_manager: DialogManager):
     # it is important to reset stack because user wants to restart everything
-    await dialog_manager.start(DialogSG.start, mode=StartMode.RESET_STACK)
+    await dialog_manager.start(DialogSG.main_menu, mode=StartMode.RESET_STACK)
 
 
 async def main():
